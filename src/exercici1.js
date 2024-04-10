8a62f2e1bb7edcb9f39c6b9eb844cdcfecab41bd
Ainhoa <ainhoa.dot@cirvianum.cat>
2024-04-10 16:27:07 +0200

    Guardar les dades en local i pasar al exercici 2.

--- src/exercici1.js
+++ src/exercici1.js
@@ -16,11 +16,13 @@ document.getElementById('registerForm').addEventListener('submit', function (eve
     //1. Tots els camps del formulari són obligatoris. Heu de mostrar un missatge d'alerta si algun camp no està omplert.
     if (!name || !surname || !email || !password || !dni || !birthdate || !gender) {
         alert('Hi ha camps del formulari buits.');
+        return;
     }
 
     //2. La contrasenya ha de tenir com a mínim 6 caràcters. Heu de mostrar un missatge d'alerta si la contrasenya és massa curta.
     if (password.length < 6) {
         alert("La contrasenya ha de tenir més de 6 caràcters.");
+        return;
     }
 
     //3. L'adreça de correu electrònic ha de tenir un format vàlid. Utilitzeu l'expressió regular proporcionada a continuació per validar l'adreça de correu electrònic:
@@ -28,6 +30,7 @@ document.getElementById('registerForm').addEventListener('submit', function (eve
 
     if (!emailRegex.test(email)) {
         alert("L'adreça de correu electrònic no és vàlida");
+        return;
     }
 
     //4. L'usuari ha de ser major d'edat per registrar-se. Aquesta validació depèn de la data de naixement proporcionada. Heu de calcular l'edat de l'usuari i mostrar un missatge d'alerta si és menor de 18 anys.
@@ -40,16 +43,39 @@ document.getElementById('registerForm').addEventListener('submit', function (eve
 
     if (ageUser < 18) {
         alert("Has de ser major d'edat per registrar-te.");
+        return;
     }
 
     //5. El camp del DNI/NIF ha de tenir un format vàlid. Utilitzeu l'expressió regular proporcionada a continuació per validar aquest camp:
     const dniRegex = /^[a-zA-Z0-9]?[0-9]{8}[a-zA-Z0-9]?$/;
-    if (!dniRegex.test(dni)){
+    if (!dniRegex.test(dni)) {
         alert('El DNI no és vàlid');
+        return;
     }
 
     //6. L'usuari ha de marcar l'opció per acceptar els Termes i Condicions. Heu de mostrar un missatge d'alerta si aquesta opció no està marcada.
     if (!conditions) {
         alert("Has d'acceptar els termes i condicions.");
+        return;
     }
+
+    //Per recuperar les dades i emmagatzemar-les (BASE DE DADES LOCAL):
+    const formData = {
+        name,
+        surname,
+        email,
+        password,
+        dni,
+        birthdate,
+        gender,
+    }
+
+    sessionStorage.setItem('formData', JSON.stringify(formData));
+
+    const storedFormData = sessionStorage.getItem('formData');
+    console.log(storedFormData);
+
+
+    //Un cop està tot correcte pasem al següent exercici (Exercici 2 - Consulta el temps meteorològic)
+    window.location.href = 'exercici2.html';
 })
